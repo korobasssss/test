58fd7edc145f25f3d0b47e934feefef645e226dd
korobasssss <an.korobeynikova@gmail.com>
2024-07-08 18:37:08 +0300

    edit WelcomePage

--- src/pages/Welcome/WelcomePage.tsx
+++ src/pages/Welcome/WelcomePage.tsx
@@ -1,15 +1,19 @@
 import React, { ReactElement, useCallback } from 'react';
-import { Button } from 'src/base/components';
+import { Button, MainLayout } from 'src/base/components';
 import styles from './styles.module.scss';
 import { getGhostByIdAction } from 'src/modules/components/actions';
+import { useNavigate } from 'react-router-dom';
 
-export const WelcomePage = (): ReactElement => {
+export const WelcomeComponent = (): ReactElement => {
+
+  const navigation = useNavigate();
+
   const onClickHandler = useCallback(() => {
     console.log('Welcome');
-  }, []);
+    navigation('/main')
+  }, [navigation]);
   return (
     <div className={styles.body}>
-      <h1>Welcome Page</h1>
       <p className={styles.description}>
         Здесь будет приложение для UI Omni ghost
       </p>
@@ -31,3 +35,17 @@ export const WelcomePage = (): ReactElement => {
     </div>
   );
 };
+
+export const WelcomePage = () : ReactElement => {
+
+  return (
+    <MainLayout
+      topTitle="Welcome Page"
+      /* eslint-disable-next-line react/no-children-prop */
+      children={
+        <WelcomeComponent />
+      }
+      withBottomNavigation={false}
+    />
+  )
+}
