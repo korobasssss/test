a53871e6df24a1ea1e72380be3b4ef8209e3e2b2
korobasssss <an.korobeynikova@gmail.com>
2024-07-09 20:46:42 +0300

    edit scrollbar

--- src/base/components/MainLayout/MainLayout.tsx
+++ src/base/components/MainLayout/MainLayout.tsx
@@ -6,6 +6,7 @@ import { ISvgIconWithClick, IWithClassName } from '../../types';
 import { HeaderPanel } from '../HeaderPanel';
 import { NavLink } from 'react-router-dom';
 import { routeHome } from 'src/base/navigation';
+import { ScrollWrapper } from 'src/base/components/ScrollWrapper';
 
 const cx = classNames.bind(styles);
 
@@ -30,66 +31,70 @@ export const MainLayout = ({
   contentClassName,
 }: IMainMenuLayoutProps): ReactElement => {
   return (
-    <div className={cx('container', className)}>
-      {/* Верхняя панель */}
-      {topTitle && (
-        <section className={styles.topPanel}>
-          <HeaderPanel
-            leftIcon={leftTopIcon}
-            title={topTitle ? topTitle : ''}
-            rightIcon={rightTopIcon}
-          />
-        </section>
-      )}
-      {/* Секция контента */}
-      <section
-        onScroll={onScroll}
-        className={cx(
-          'content',
-          {
-            content_withTop: topTitle,
-            content_withBottomMenu: withBottomNavigation,
-          },
-          contentClassName,
-        )}
-      >
-        {children}
-      </section>
-      {/* Нижнее меню */}
-      {withBottomNavigation && (
-        <section className={styles.bottomMenu}>
-          <ul>
-            <li>
-              <NavLink to={routeHome.url}>
-                {({ isActive }) => (
-                  <div
-                    data-uitest="link-map"
-                    className={cx('linkBtn', {
-                      linkBtn_active: isActive,
-                    })}
-                  >
-                    <p className={styles.text}>Главная</p>
-                  </div>
-                )}
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to={routeHome.url}>
-                {({ isActive }) => (
-                  <div
-                    data-uitest="link-account"
-                    className={cx('linkBtn', {
-                      linkBtn_active: isActive,
-                    })}
-                  >
-                    <p className={styles.text}>Профиль</p>
-                  </div>
-                )}
-              </NavLink>
-            </li>
-          </ul>
-        </section>
-      )}
-    </div>
+      <div className={cx('container', className)}>
+
+          {/* Верхняя панель */}
+          {topTitle && (
+            <section className={styles.topPanel}>
+              <HeaderPanel
+                leftIcon={leftTopIcon}
+                title={topTitle ? topTitle : ''}
+                rightIcon={rightTopIcon}
+              />
+            </section>
+          )}
+          {/* Секция контента */}
+          <section
+            onScroll={onScroll}
+            className={cx(
+              'content',
+              {
+                content_withTop: topTitle,
+                content_withBottomMenu: withBottomNavigation,
+              },
+              contentClassName,
+            )}
+          >
+            <ScrollWrapper>
+              {children}
+            </ScrollWrapper>
+          </section>
+          {/* Нижнее меню */}
+          {withBottomNavigation && (
+            <section className={styles.bottomMenu}>
+              <ul>
+                <li>
+                  <NavLink to={routeHome.url}>
+                    {({ isActive }) => (
+                      <div
+                        data-uitest="link-map"
+                        className={cx('linkBtn', {
+                          linkBtn_active: isActive,
+                        })}
+                      >
+                        <p className={styles.text}>Главная</p>
+                      </div>
+                    )}
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to={routeHome.url}>
+                    {({ isActive }) => (
+                      <div
+                        data-uitest="link-account"
+                        className={cx('linkBtn', {
+                          linkBtn_active: isActive,
+                        })}
+                      >
+                        <p className={styles.text}>Профиль</p>
+                      </div>
+                    )}
+                  </NavLink>
+                </li>
+              </ul>
+            </section>
+          )}
+
+      </div>
   );
 };
