c4e451668d7a5380a9f814426fe51ddbbefbcdcd
korobasssss <an.korobeynikova@gmail.com>
2024-08-20 16:27:23 +0300

    add ListWithLineWrapper to DataListComponent

--- src/modules/components/components/DataListComponent/DataListComponent.tsx
+++ src/modules/components/components/DataListComponent/DataListComponent.tsx
@@ -6,6 +6,7 @@ import cx from 'classnames';
 import { EDeviceStatus, IDataView } from 'src/modules/components';
 import { isArray } from 'src/base';
 import { TextNoData } from 'src/base/components/TextNoData/TextNoData';
+import { ListWithLineWrapper } from 'src/base/components';
 
 interface IDataList {
   data: IDataView[];
@@ -31,23 +32,23 @@ export const DataListComponent: FC<IDataList> = ({
     isArray(filteredDataByStatus) ?
       filteredDataByStatus.map((itemMap, index) => (
         <li key={itemMap.id} className={style.list_section}>
-          <div className={style.list_item}>
-            <div className={style.id_section}>
-              <div className={style.num}>{index + 1}</div>
-              <div className={style.data}>{itemMap.id}</div>
+          <ListWithLineWrapper
+            isLastItem={!(index !== filteredDataByStatus.length - 1)}
+            margin='small'>
+            <div className={style.list_item}>
+              <div className={style.id_section}>
+                <div className={style.num}>{index + 1}</div>
+                <div className={style.data}>{itemMap.id}</div>
+              </div>
+              <Link to={routeComponentsView.url({ id: itemMap.id })} className={style.status_button}>
+                <div>{itemMap.status}</div>
+                <div className={cx({
+                  [style.online]: itemMap.status === EDeviceStatus.ONLINE,
+                  [style.offline]: itemMap.status === EDeviceStatus.OFFLINE,
+                })} />
+              </Link>
             </div>
-            <Link to={routeComponentsView.url({ id: itemMap.id })} className={style.status_button}>
-              <div>{itemMap.status}</div>
-              <div className={cx({
-                [style.online]: itemMap.status === EDeviceStatus.ONLINE,
-                [style.offline]: itemMap.status === EDeviceStatus.OFFLINE,
-              })} />
-            </Link>
-          </div>
-          {
-            index !== filteredDataByStatus.length - 1 ?
-              <span className={style.ul_border} />
-              : null}
+          </ListWithLineWrapper>
         </li>
       ))
       :
